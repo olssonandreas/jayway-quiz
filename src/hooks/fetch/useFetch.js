b2fb1f854f7d6084a68df90d69505b84874a5caa
Andreas Olsson <andreas.olsson@kan.se>
2020-04-05 20:48:00 +0200

    Fix eslint missing dependency error

--- src/hooks/fetch/useFetch.js
+++ src/hooks/fetch/useFetch.js
@@ -1,13 +1,13 @@
 import { useState, useEffect } from 'react';
 
-export default (url, options) => {
+export default (url) => {
   const [response, setResponse] = useState(null);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await fetch(url, options);
+        const res = await fetch(url);
         const json = await res.json();
         setResponse(json);
       } catch (error) {
@@ -15,7 +15,7 @@ export default (url, options) => {
       }
     };
     fetchData();
-  }, []);
+  }, [url]);
 
   return { response, error };
 };
